6f9df85369bc19f5e390a16d4b69f25df9ea29a2
Md Sohail Ansari <sohailworks10@gmail.com>
2025-09-25 21:01:54 +0530

    changed cors

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:3000"],  # React dev servers
+    allow_origins=["http://localhost:5173", "https://jobtracker.heysohail.xyz"],  # React dev servers
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
